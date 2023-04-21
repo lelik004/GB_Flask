89292c16a0a8375375ec5069de5ab8a7b649efb6
Aleksei Abakumov <alekseiabakumov@MacBook-Air-Aleksei.local>
2023-04-21 20:48:58 +0400

    Задания ко второму уроку

--- blog/app.py
+++ blog/app.py
@@ -1,53 +1,83 @@
-from time import time
+from flask import Flask, render_template
 
-from flask import Flask, request, g
-
-app = Flask(__name__)
-
-
-@app.route('/<string:some_var>')
-def index(some_var: str):
-    return f'Hello, {some_var}'
+from blog.articles.views import articles_app
+from blog.users.views import users_app
 
 
-@app.route('/query_string/')
-def query_string():
-    first_var = request.args.get('first')
-    second_var = request.args.get('second')
-    return f'{first_var} and {second_var}'
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 
-@app.route('/custom_status/', methods=['GET', 'POST'])
-def custom_status_code():
-    if request.method == 'GET':
-        return '''\
-        Чтобы получить ответ с кастомным статусом
-        отправь запрос использую миетод POST и передай "code" в JSON/FormData
-        '''
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
 
-    print('raw data:', request.data)
 
-    if request.form and 'code' in request.form:
-        return 'code from form', request.form['code']
+def register_blueprints(app: Flask):
+    app.register_blueprint(users_app)
+    app.register_blueprint(articles_app)
 
-    if request.json and 'code' in request.json:
-        return 'code from json', request.json['code']
 
-    return "", 204
 
-@app.before_request
-def process_before_request():
-    g.start_time = time()
 
-@app.after_request
-def process_after_request(response):
-    if hasattr(g, 'start_time'):
-        response.headers['process-time'] = time() - g.start_time
 
-    return response
 
-@app.errorhandler(404)
-def handler_404(error):
-    app.logger.error(error)
-    return 'Error 404'
 
+
+
+
+'''
+Lesson 1
+'''
+# from time import time
+# from flask import request, g
+#
+# @app.route('/<string:some_var>')
+# def index(some_var: str):
+#     return f'Hello, {some_var}'
+#
+#
+# @app.route('/query_string/')
+# def query_string():
+#     first_var = request.args.get('first')
+#     second_var = request.args.get('second')
+#     return f'{first_var} and {second_var}'
+#
+#
+# @app.route('/custom_status/', methods=['GET', 'POST'])
+# def custom_status_code():
+#     if request.method == 'GET':
+#         return
+#         '''\
+#         Чтобы получить ответ с кастомным статусом
+#         отправь запрос использую миетод POST и передай "code" в JSON/FormData
+#         '''
+#
+#     print('raw data:', request.data)
+#
+#     if request.form and 'code' in request.form:
+#         return 'code from form', request.form['code']
+#
+#     if request.json and 'code' in request.json:
+#         return 'code from json', request.json['code']
+#
+#     return "", 204
+#
+# @app.before_request
+# def process_before_request():
+#     g.start_time = time()
+#
+# @app.after_request
+# def process_after_request(response):
+#     if hasattr(g, 'start_time'):
+#         response.headers['process-time'] = time() - g.start_time
+#
+#     return response
+#
+# @app.errorhandler(404)
+# def handler_404(error):
+#     app.logger.error(error)
+#     return 'Error 404'
+#
---
+++ blog/articles/views.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
+
+articles_app = Blueprint('articles_app', __name__, url_prefix='/articles', static_folder='../static')
+
+ARTICLES = {1: {'title': 'FLASK',
+                'body': 'FLASK_body'},
+            2: {'title': 'DJANGO',
+                'body': 'DJANGO_body'},
+            3: {'title': 'PYTHON',
+                'body': 'PYTHON_body'},
+            4: {'title': 'HTML/CSS',
+                'body': 'HTML/CSS_body'}
+            }
+
+
+@articles_app.route('/', endpoint='list')
+def articles_list():
+    return render_template('articles/list.html', articles=ARTICLES)
+
+
+@articles_app.route('/<int:article_id>/', endpoint='details')
+def article_detail(article_id: int):
+    try:
+        article = ARTICLES[article_id]
+    except KeyError:
+        raise NotFound(f'Статья с ID {article_id} не существует')
+    return render_template('articles/details.html', article_id=article_id, article=article)
